12dbbddf27f5a59031baf6d608a711e50df0b64a
jayatikolay <jayatii.kolaay@gmail.com>
2023-03-14 00:02:45 +0100

    added swagger related config

--- src/main/java/com/carlease/customer/securityConfig/ProjectSecurityConfig.java
+++ src/main/java/com/carlease/customer/securityConfig/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import com.carlease.customer.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -60,4 +61,8 @@ public class ProjectSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**");
+    }
 }
